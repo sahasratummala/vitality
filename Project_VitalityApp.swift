dfbaa3805bae6adf398341655deac8ef7d555e1d
Sahasra Tummala <76259284+sahasratummala@users.noreply.github.com>
2023-06-27 16:23:08 -0500

    Add files via upload

---
+++ Project_VitalityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_VitalityApp.swift
+//  Project Vitality
+//
+//  Created by Sahasra Tummala on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Project_VitalityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project_VitalityUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Project_VitalityUITestsLaunchTests.swift
+//  Project VitalityUITests
+//
+//  Created by Sahasra Tummala on 6/27/23.
+//
+
+import XCTest
+
+final class Project_VitalityUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
